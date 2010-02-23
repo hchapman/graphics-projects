4802f035cb6a86bd9bc2fe9d86e5c481b6c7393a
Harrison Chapman <hchapman@bowdoin.edu>
2010-02-23 15:40:38 -0500

    We only need Vector3s in updateViewMatrix, I believe.

--- src/re330/Camera.cpp
+++ src/re330/Camera.cpp
@@ -2,24 +2,20 @@
 
 namespace RE330 {
     void Camera::updateViewMatrix() {
-          
-        Vector4 c = (e-p).normalize(); // The z_c column vector
+        Vector3 a,b,c,d;  
+        c = (e-p).asVector3().normalize(); // The z_c column vector
         // x_c is the cross product of up and z_c, normalized
-        Vector4 a = Vector4(u.asVector3() * c.asVector3(), 0).normalize();
+        a = (u.asVector3() * c).normalize();
         // y_c is the cross product of z_c and x_c, normalized
-        Vector4 b = Vector4(c.asVector3() * a.asVector3(), 0).normalize();   
+        b = (c * a).normalize();   
         // The translation column
-        Vector4 d = Vector4(-e[0], -e[1], -e[2], 1); 
-        
-        Vector3 tmp =  b.asVector3()*c.asVector3();
-        std::cout << tmp << std::endl;
-        std::cout << a << b << c << d << std::endl;
+        d = e.asVector3() * -1; 
 
         // Finally, change the view matrix
         v = Matrix4(a[0], b[0], c[0], d[0],
                     a[1], b[1], c[1], d[1],
                     a[2], b[2], c[2], d[2],
-                    a[3], b[3], c[3], d[3]);
+                    0, 0, 0, 1);
         // v = Matrix4(a[0], b[0], c[0], 0,
         //             a[1], b[1], c[1], 0,
         //             a[2], b[2], c[2], 0,
